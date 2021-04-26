2cbd23c0ffa1966f777e51fecda1debdd6e6a342
Ramna Sidharta <ramna.thors@gmail.com>
2021-04-25 21:01:24 -0300

    Internacionalize component Info

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Info.css';
+import { FormattedMessage } from 'react-intl';
+import { i18nConfig, language } from '../../i18n.js';
 
 const Info = ({ weight, height, id, date, bmi, deleteCard }) => {
   const handleDelete = () => {
     deleteCard(id);
   };
 
+  const i18nMessages = i18nConfig.messages[language];
+
   return (
     <div className="col m6 s12">
       <div className="card">
         <div className="card-content">
           <span className="card-title" data-test="bmi">
-            BMI: {bmi}
+             {i18nMessages.bmi}: {bmi}
           </span>
           <div className="card-data">
-            <span data-test="weight">Weight: {weight} kg</span>
-            <span data-test="height">Height: {height} cm</span>
-            <span data-test="date">Date: {date}</span>
+            <span class="fl" data-test="weight">
+              {i18nMessages.weight}: {weight} {i18nMessages.weightMeasure}
+            </span>
+            <span class="fl" data-test="height">
+              {i18nMessages.height}: {height} {i18nMessages.heightMeasure}
+            </span>
+            <span class="fl" data-test="date">
+              {i18nMessages.date}: {date}
+            </span>
           </div>
 
           <button className="delete-btn" onClick={handleDelete}>
